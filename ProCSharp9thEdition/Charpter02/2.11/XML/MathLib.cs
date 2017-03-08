da85fe3d3ed2ebf5914bf7673cacc6054dd6a3d6
zallerting <zallerting@gmail.com>
2017-03-08 19:59:14 +0800

    Charpter 2, Section 2.11.
    Pro C# learning codings of Charpter 2, Section 2.11.

---
+++ ProCSharp9thEdition/Charpter02/2.11/XML/MathLib.cs
@@ -0,0 +1,21 @@
+// MathLib.cs
+namespace Wrox
+{
+    /// <summary>
+    /// Wrox.Math class.
+    /// Provides a method to add two integers.
+    /// </summary>
+	public class MathLib
+	{
+        /// <summary>
+        /// The Add method allows us to add two integers.
+        /// </summary>
+        /// <param name="x">First number to add</param>
+        /// <param name="y">Second number to add</param>
+        /// <returns>Result of the addition(int)</returns>
+		public int Add(int x, int y)
+		{
+			return x + y;
+		}
+	}
+}
