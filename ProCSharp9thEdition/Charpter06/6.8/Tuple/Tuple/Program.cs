8aa1988a462a899b1dfd2b37be8019be6ba226e6
zallerting <zallerting@gmail.com>
2017-03-20 15:30:57 +0800

    Charpter 6, Section 8 and 9.
    Add learning codes of Charpter 6, Section 8 and 9 about Pro C#(9th
    Edition).

---
+++ ProCSharp9thEdition/Charpter06/6.8/Tuple/Tuple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleTest
+{
+    class Program
+    {
+        // 创建元组的Divide()方法
+        public static Tuple<int, int> Divide(int dividend, int divisor)
+        {
+            int result = dividend / divisor;
+            int reminder = dividend % divisor;
+            // 元组用静态Tuple类的Create()方法创建
+            // Create()方法的泛型参数定义了要实例化的元组类型
+            // 新建的元组用result和reminder变量初始化
+            return Tuple.Create(result, reminder);
+        }
+        static void Main(string[] args)
+        {
+            // 调用Divide()方法
+            var result = Divide(5, 2);
+            // result.Item1:获取当前Tuple<T1, T2>对象的第一个分量的值
+            // result.Item2:获取当前Tuple<T1, T2>对象的第二个分量的值
+            Console.WriteLine("result of division: {0}, reminder: {1}", result.Item1, result.Item2);
+            // 创建八元组, 最后一个模板参数为TRest，表示传递一个元组，这样就可以创建带任意个参数的元组了
+            var tuple = Tuple.Create("Stephanie", "Aina", "Nagel", 2009, 6, 2, 1.37, Tuple.Create(52, 3490));
+            // 输出八元组各个分量对应的值
+            Console.WriteLine("八元组各分量的值依次为：第一分量：{0} 第二分量：{1} 第三分量：{2} 第四分量：{3} 第五分量：{4} 第六分量：{5} 第七分量：{6} 剩余分量：{7} ", 
+                tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Rest);
+            Console.Read();
+        }
+    }
+}
---
+++ ProCSharp9thEdition/Charpter06/6.9/StructureCompare/StructureCompare/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StructureCompare
+{
+    class Person : IEquatable<Person>
+    {
+        // 创建Id、FirstName、LastName属性
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // 定义强类型化Equals()方法
+        public bool Equals(Person other)
+        {
+            if (other == null)
+                return base.Equals(other);
+            return this.Id == other.Id && this.FirstName == other.FirstName
+                && this.LastName == other.LastName;
+        }
+        // 重写IEquatable<Person>的Equals()方法
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return base.Equals(obj);
+            // 调用强类型化的Equals()方法
+            return this.Equals(obj as Person);
+        }
+        // 重写Object的ToString()方法
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", this.Id, this.FirstName, this.LastName); 
+        }
+        // 重写Object的GetHashCode()方法
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+    // IEqualityComparer：定义方法以支持对象的相等比较
+    class TupleComparer : IEqualityComparer 
+    {
+        // 实现IEqualityComparer接口的Equals()方法
+        // 因为Object类也定义了带两个参数的静态Equals()方法，
+        // 所以此处实现的Equals()方法需要new关键字修饰
+        public new bool Equals(object x, object y)
+        {
+            return x.Equals(y);
+        }
+        // 实现IEqualityComparer接口的GetHashCode()方法
+        public int GetHashCode(object obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+    class MainEntryPoint
+    {
+        static void Main(String[] args)
+        {
+            // 创建一个Person类对象
+            var janet = new Person { FirstName = "janet", LastName = "Jackson" };
+            // 创建两个内容相同的不同Person类对象
+            Person[] person1 = {
+                new Person {
+                    FirstName = "Michael",
+                    LastName = "Jackson"
+                },
+                janet
+            };
+            Person[] person2 = {
+                new Person {
+                    FirstName = "Michael",
+                    LastName = "Jackson"
+                },
+                janet
+            };
+            // 比较两个不同的数组，引用不同故不相等
+            if (person1 != person2)
+            {
+                Console.WriteLine("person1 has not the same reference with person2.");
+            }
+            // 比较两个不同数组的内容，内容相同故相等
+            // IStructuralEquatable接口定义的Equals()方法，第一个参数为Object类型，第
+            // 二个参数为IEqualityComparer类型。通过EqualityComparer<T>类完成IEqualityComparer
+            // 的一个默认实现，返回一个默认的相等比较器，用于比较此泛型参数指定的类型。
+            if ((person1 as IStructuralEquatable).Equals(person2,
+                EqualityComparer<Person>.Default))
+            {
+                Console.WriteLine("person1 has the same content with person2.");
+            }
+            // 创建两个内容相同的元组实例
+            var t1 = Tuple.Create<int, string>(1, "Stephanie");
+            var t2 = Tuple.Create<int, string>(1, "Stephanie");
+            // 比较两个不同二元组，引用不同故不相等
+            if (t1 != t2)
+            {
+                Console.WriteLine("t1 has not the same reference with t2.");
+            }
+            // 比较元组内容，内容相同故相等
+            if (t1.Equals(t2))
+            {
+                Console.WriteLine("t1 has the same content with t2.");
+            }
+            // IStructuralEquatable：定义方法以支持对象的结构相等性比较
+            if ((t1 as IStructuralEquatable).Equals(t2, new TupleComparer()))
+            {
+                Console.WriteLine("equals using TupleComparer.");
+            }
+
+            Console.Read();
+        }
+    }
+}
